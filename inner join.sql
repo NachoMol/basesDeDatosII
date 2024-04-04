9281e211e28754133177a876aece32b2e857799d
NachoMol <ignaciotmolina@gmail.com>
2024-04-04 19:58:10 -0300

    agregando alias a los resultados

--- inner join.sql
+++ inner join.sql
@@ -1,4 +1,4 @@
 -- Traer todos los productos y sus categorias
-SELECT p.name , p.stock, c.name
+SELECT p.name AS product_name, p.stock, c.name AS category_name
 FROM products AS p
 JOIN categories AS c ON p.category_id = c.id;
