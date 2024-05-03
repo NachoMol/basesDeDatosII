7116dc43a11dfed732b27249cf0b1cb4af2ded7c
NachoMol <ignaciotmolina@gmail.com>
2024-05-03 18:50:51 -0300

    overview store procedures

---
+++ create store procedure.sql
@@ -0,0 +1,15 @@
+-- crear
+DELIMITER $$
+CREATE PROCEDURE nameProcedure()
+BEGIN
+-- instrucciones sql
+
+END$$
+DELIMITER ;
+
+-- llamar
+CALL nameProcedure();
+
+-- eliminar
+
+DROP PROCEDURE nameProcedure;
