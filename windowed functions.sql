91573228d3cca2e95d7d4b05449539bdaf7a16a1
NachoMol <ignaciotmolina@gmail.com>
2024-05-06 19:32:01 -0300

    agregando suma de stock particionada

--- windowed functions.sql
+++ windowed functions.sql
@@ -1,6 +1,7 @@
 -- Obtener un listado de productos y adicionar en cada fila la posicion segun la categoria
 SELECT
-name, category_id,
-ROW_NUMBER() OVER (PARTITION BY category_id order by category_id, name)
+name, category_id
+,ROW_NUMBER() OVER (PARTITION BY category_id order by category_id, name) AS orderByCategory
+, SUM(stock) OVER(PARTITION BY category_id) AS categoryStock
 FROM products
 ORDER BY category_id, name;
