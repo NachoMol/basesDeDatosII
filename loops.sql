f5007f32c608c118e8c00078c0397d2ce98749fc
NachoMol <ignaciotmolina@gmail.com>
2024-05-06 20:56:03 -0300

    implementando while

--- loops.sql
+++ loops.sql
@@ -1,16 +1,34 @@
+-- FOR
 DELIMITER $$
 CREATE PROCEDURE sayHelloNTimes(times INT)
 BEGIN
-	DECLARE counter INT DEFAULT 0;
+	DECLARE counter INT DEFAULT 1;
     my_loop: LOOP
 		SELECT "hello";
-        set counter = counter + 1;
+        SET counter = counter + 1;
         IF counter > times THEN
         LEAVE my_loop;
         END IF;
 	END LOOP;
 END $$;
 DELIMITER ;
-CALL sayHelloNTimes(3);
+CALL sayHelloNTimes(0);
 
 DROP PROCEDURE sayHelloNTimes;
+
+-- WHILE
+DELIMITER $$
+CREATE PROCEDURE sayHelloNTimes(times INT)
+BEGIN
+	DECLARE counter INT DEFAULT 0;
+    WHILE counter < times DO
+    SELECT "hello";
+    SET counter = counter + 1;
+    END WHILE;
+END $$;
+DELIMITER ;
+
+CALL sayHelloNTimes(2);
+DROP PROCEDURE sayHelloNTimes;
+
+-- REPEAT
