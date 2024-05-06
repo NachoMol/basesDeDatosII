d97ffb523b97005ba25bc67adbcb9e8074b28005
NachoMol <ignaciotmolina@gmail.com>
2024-05-06 19:37:02 -0300

    agregando lag function

--- windowed functions.sql
+++ windowed functions.sql
@@ -3,6 +3,7 @@ SELECT
 name, category_id
 ,ROW_NUMBER() OVER (w order by category_id, name) AS orderByCategory
 , SUM(stock) OVER(w) AS categoryStock
+, LAG(name) OVER(w order by category_id, name)
 FROM products
 WINDOW w AS (PARTITION BY category_id)
 ORDER BY category_id, name ASC
